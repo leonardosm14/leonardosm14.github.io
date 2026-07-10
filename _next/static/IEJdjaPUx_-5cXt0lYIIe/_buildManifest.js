7472d333c06f1ad5c6c58699fd0fa6ce91e697a4
leonardosm14 <leosouzaam@hotmail.com>
2026-07-09 21:02:10 -0300

    Updates

---
+++ _next/static/IEJdjaPUx_-5cXt0lYIIe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-6d4db8c22fc9b5f5.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
